d12373273e23299bbd4a24b3a4a0fe75e02a65b0
JosephOlabode <josepholabode17@gmail.com>
2022-04-06 20:30:12 +0100

    implemented the delete command for order api

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandValidator: AbstractValidator<DeleteOrderCommand>
+    {
+        public DeleteOrderCommandValidator()
+        {
+            RuleFor(p => p.Id).NotEmpty().WithMessage("{Id} is required.");
+        }
+    }
+}
