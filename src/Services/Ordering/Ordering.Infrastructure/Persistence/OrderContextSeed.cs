08feba2fc3e644f4967e66eb3162ab3c471eb435
JosephOlabode <josepholabode17@gmail.com>
2022-04-13 22:32:09 +0100

    implemented the repository implementation and the mail sending

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public OrderContextSeed()
+        {
+        }
+
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if(!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+
+
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "swn", FirstName = "Joseph", LastName = "Olabode", EmailAddress = "josepholabode14@gmail.com", AddressLine = "Iwo", Country = "Nigeria", TotalPrice = 350 };
+            }
+        }
+    }
+}
